c98779dd13f30acaf9d650f56826d4e79f1fe6d4
tensor <drew.tensor@gmail.com>
2022-02-27 06:58:16 -0800

    added some repo state management scripts for new branches and cloning repositories

---
+++ repo-cloner.js
@@ -0,0 +1,28 @@
+require('colors');
+const path = require('path');
+const shell = require('shelljs');
+const { repositories } = require('./config');
+
+const repositoriesDirectory = path.join(__dirname, 'my-repositories');
+
+function cloneRepositories(repositoryPath, repositoryList = []) {
+  const repositoryCount = repositoryList.length;
+
+  if (!repositoryPath || repositoryCount === 0) {
+    console.log('Invalid path or repository list');
+    return;
+  }
+
+  console.log(`\nCloning repositories to: ${repositoriesDirectory}`.blue);
+
+  shell.cd(repositoryPath);
+
+  repositoryList.forEach((repositoryUrl, index) => {
+    console.log(`Cloning ${index + 1} of ${repositoryCount}`.cyan);
+    shell.exec(`git clone ${repositoryUrl} --progress -b master`);
+  });
+
+  console.log('\nCompleted cloning of repositories'.green + '\n');
+}
+
+cloneRepositories(repositoriesDirectory, repositories);
